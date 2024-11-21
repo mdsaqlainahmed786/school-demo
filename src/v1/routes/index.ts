149a5bc7bdecc3f64ad89ddc4d5d736b3628948b
Saqlain <riyanahmed1703@gmail.com>
2024-11-21 19:18:35 +0530

    updated the port

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import schoolRouter from './schools';
 const app = express();
+const port = process.env.PORT || 5002;
 app.use(cors());
 app.use(express.json());
 // Mount the school router on the /api/v1/schools route
@@ -9,6 +10,6 @@ app.use('/api/v1/schools', schoolRouter);
 
 
 
-app.listen(5002, () => {
-    console.log('Server is running on port 5002');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
